e5460098bf3e46a64796a7e38104827eedb16170
James Knight <james.d.knight@live.com>
2019-02-10 22:17:40 -0500

    initial build support
    Introduces the initial build support for the releng-tool project. This
    includes `setup.py` as well as `tox`-related configurations.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2018 releng-tool
+
+from setuptools import find_packages
+from setuptools import setup
+import os
+
+def read(name):
+    return open(os.path.join(os.path.dirname(__file__), name)).read()
+
+setup(
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    description='release engineering utility tool',
+    entry_points={
+        'console_scripts': [
+            'releng-tool = releng.__main__:main',
+        ],
+    },
+    install_requires=[
+        "enum34; python_version == '2.7'"
+    ],
+    license='BSD-2-Clause',
+    long_description=read('README'),
+    name='releng-tool',
+    packages=find_packages(exclude=["test*"]),
+    platforms='any',
+    test_suite='test',
+    url='https://github.com/releng-tool/releng-tool',
+    version='0.1.0.dev0',
+    zip_safe=False,
+)
