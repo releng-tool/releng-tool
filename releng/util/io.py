e78c5e55fb74c97073f6185eb8eaabfa1900e18c
James Knight <james.d.knight@live.com>
2019-02-12 22:02:27 -0500

    util: introduce a touch utility method
    Introducing a `touch` utility method to perform in a similar fashion as
    the Linux common `touch` command. This is in preparation to cleanup some
    internal implementation as well as prepare for a new script helper.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- releng/util/io.py
+++ releng/util/io.py
@@ -316,6 +316,37 @@ def prependShebangInterpreter(args):
         pass
     return args
 
+def touch(file):
+    """
+    update a file's access/modifications times
+
+    Attempts to update the access/modifications times on a file. If the file
+    does not exist, it will be created. This utility call operates in the same
+    fashion as the ``touch`` system command.
+
+    An example when using in the context of script helpers is as follows:
+
+    .. code-block:: python
+
+        if releng_touch('my-file'):
+            print('file was created')
+        else:
+            print('file was not created')
+
+    Args:
+        file: the file
+
+    Returns:
+        ``True`` if the file was created/updated; ``False`` if the file could
+        not be created/updated
+    """
+    try:
+        with open(file, 'a'):
+            os.utime(file, None)
+        return True
+    except:
+        return False
+
 def execute(args, cwd=None, env=None, env_update=None, quiet=False,
         critical=True, poll=False, capture=None):
     """
