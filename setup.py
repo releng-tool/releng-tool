9c506a315d71397540078ccc105a8f9c1d981844
James Knight <james.d.knight@live.com>
2019-02-13 00:15:09 -0500

    releng-tool 0.1.0.dev2
    Signed-off-by: James Knight <james.d.knight@live.com>

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     platforms='any',
     test_suite='test',
     url='https://github.com/releng-tool/releng-tool',
-    version='0.1.0.dev1',
+    version='0.1.0.dev2',
     zip_safe=False,
 )
