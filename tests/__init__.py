1f45b63b17e7796281bfd9706743f314abff5fce
James Knight <james.d.knight@live.com>
2021-04-10 00:46:00 -0400

    tests: adding initial engine unit test examples
    Adding the first set of unit tests which help validate a prepared and
    invoked `RelengEngine` instance. Unit testing primarily consisted of
    testing utility and other leaf-related implementation.
    
    This commit provides some support calls to help a unit test prepare a
    working directory to invoke an engine instance on. In addition, this
    test also provides validation of the recent changes [1] where exceptions
    could be raised for some unique configuration scenarios.
    
    [1]: 8fc96558e3283f406a0c60dff1cb21565365bb8d
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,9 +2,13 @@
 # Copyright 2018-2021 releng-tool
 
 from contextlib import contextmanager
-from releng_tool.util.io import generate_temp_dir
 from difflib import unified_diff
 from io import open
+from releng_tool.engine import RelengEngine
+from releng_tool.opts import RelengEngineOptions
+from releng_tool.util.io import generate_temp_dir
+from releng_tool.util.io import path_copy
+import os
 import sys
 
 try:
@@ -52,6 +56,61 @@ def compare_contents(first, second):
     return None
 
 @contextmanager
+def prepare_testenv(config=None, template=None, args=None):
+    """
+    prepare an engine-ready environment for a test
+
+    This utility method is used to provide an `RelengEngine` instance ready for
+    execution on an interim working directory.
+
+    Args:
+        config (optional): dictionary of options to mock for arguments
+        template (optional): the folder holding a template project to copy into
+                              the prepared working directory
+        args (optional): additional arguments to add to the "forwarded options"
+
+    Yields:
+        the engine
+    """
+
+    class MockArgs(object):
+        def __getattr__(self, name):
+            return self.name if name in self.__dict__ else None
+
+    if config is None:
+        config = {}
+
+    with generate_temp_dir() as work_dir:
+        # force root directory to temporary directory; or configure all working
+        # content based off the generated temporary directory
+        if 'root_dir' not in config:
+            config['root_dir'] = work_dir
+        else:
+            if 'cache_dir' not in config:
+                config['cache_dir'] = os.path.join(work_dir, 'cache')
+            if 'dl_dir' not in config:
+                config['dl_dir'] = os.path.join(work_dir, 'dl')
+            if 'out_dir' not in config:
+                config['out_dir'] = os.path.join(work_dir, 'out')
+
+        if template:
+            test_base = os.path.dirname(os.path.realpath(__file__))
+            templates_dir = os.path.join(test_base, 'templates')
+            template_dir = os.path.join(templates_dir, template)
+            path_copy(template_dir, work_dir)
+
+        # build arguments instance
+        test_args = MockArgs()
+        for k, v in config.items():
+            setattr(test_args, k, v)
+
+        # prepare engine options and build an engine instance
+        opts = RelengEngineOptions(args=test_args, forward_args=args)
+        engine = RelengEngine(opts)
+
+        yield engine
+
+@contextmanager
 def prepare_workdir():
     """
     prepare a working directory for a test
@@ -89,3 +148,25 @@ def redirect_stdout(new_target=None):
         yield new_target
     finally:
         sys.stdout = _
+
+def run_testenv(config=None, template=None, args=None):
+    """
+    execute an engine instance with provide environment options for a test
+
+    This utility method is used to invoke an `RelengEngine` instance which is
+    prepared based off the provided configuration options and base template (if
+    any) to copy.
+
+    Args:
+        config (optional): dictionary of options to mock for arguments
+        template (optional): the folder holding a template project to copy into
+                              the prepared working directory
+        args (optional): additional arguments to add to the "forwarded options"
+
+    Returns:
+        the engine
+    """
+
+    with prepare_testenv(config=config, template=template, args=args) as engine:
+        engine.run()
+    return engine
---
+++ tests/unit-tests/test_engine_run_defaults.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 releng-tool
+
+from releng_tool.exceptions import MissingConfigurationError
+from releng_tool.exceptions import MissingPackagesError
+from tests import prepare_testenv
+from tests import run_testenv
+import os
+import unittest
+
+class TestEngineRunDefaults(unittest.TestCase):
+    def test_engine_run_defaults_dirs(self):
+        with prepare_testenv(template='minimal') as engine:
+            # check if root directory is tracked
+            root_dir = engine.opts.root_dir
+            self.assertTrue(os.path.exists(root_dir))
+
+            # ensure default paths for other directories are populated
+            expected_cache_dir = os.path.join(root_dir, 'cache')
+            expected_dl_dir = os.path.join(root_dir, 'dl')
+            expected_out_dir = os.path.join(root_dir, 'output')
+            self.assertEqual(engine.opts.cache_dir, expected_cache_dir)
+            self.assertEqual(engine.opts.dl_dir, expected_dl_dir)
+            self.assertEqual(engine.opts.out_dir, expected_out_dir)
+
+    def test_engine_run_defaults_noconfig(self):
+        with self.assertRaises(MissingConfigurationError):
+            run_testenv()
+
+    def test_engine_run_defaults_nopackages(self):
+        with self.assertRaises(MissingPackagesError):
+            run_testenv(template='no-packages')
