276b5688f029fcb2cc4a6bc2f38f13bc2711920a
James Knight <james.d.knight@live.com>
2021-04-17 11:43:27 -0400

    util: adding nullish coalescing utility call
    Provides a utility call to assist in providing a default value for an
    assignment when a provided value is detected to be a `None` value. This
    has been added to help minimize statements needing to apply a default on
    a `None` detection.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- releng_tool/util/__init__.py
+++ releng_tool/util/__init__.py
@@ -1,1 +1,21 @@
 # -*- coding: utf-8 -*-
+# Copyright 2021 releng-tool
+
+def nullish_coalescing(value, default):
+    """
+    nullish coalescing utility call
+
+    Provides a return of the provided value unless the value is a ``None``,
+    which instead the provided default value is returned instead.
+
+    Args:
+        value: the value
+        default: the default value
+
+    Returns:
+        the provided value if not ``None``; otherwise, the default value
+    """
+    if value is None:
+        return default
+
+    return value
