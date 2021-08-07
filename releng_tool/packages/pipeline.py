b1b4b51130a089ecdf83cf8d3f3df8e0175db1a2
James Knight <james.d.knight@live.com>
2021-08-07 16:52:40 -0400

    packages: reorder imports
    Signed-off-by: James Knight <james.d.knight@live.com>

--- releng_tool/packages/pipeline.py
+++ releng_tool/packages/pipeline.py
@@ -21,11 +21,11 @@ from releng_tool.packages.exceptions import RelengToolPatchStageFailure
 from releng_tool.packages.exceptions import RelengToolPostStageFailure
 from releng_tool.util.file_flags import FileFlag
 from releng_tool.util.file_flags import check_file_flag
+from releng_tool.util.file_flags import process_file_flag
+from releng_tool.util.io import ensure_dir_exists
 from releng_tool.util.io import path_copy
 from releng_tool.util.log import err
 from releng_tool.util.log import warn
-from releng_tool.util.io import ensure_dir_exists
-from releng_tool.util.file_flags import process_file_flag
 import os
 
 class RelengPackagePipeline:
