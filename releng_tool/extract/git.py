263945e9f8cc69fa81551ae4a8e7cda9bc0eaba0
James Knight <james.d.knight@live.com>
2021-08-15 21:30:25 -0400

    extract: ensure diverged git repository uses remote revision
    In the event that a package is configured for a revision where the
    revision on the remote differs from a local cached repository (if any),
    ensure that the remote revision is used instead.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- releng_tool/extract/git.py
+++ releng_tool/extract/git.py
@@ -2,8 +2,10 @@
 # Copyright 2018-2021 releng-tool
 
 from releng_tool.tool.git import GIT
+from releng_tool.util.log import debug
 from releng_tool.util.log import err
 from releng_tool.util.log import log
+from releng_tool.util.log import warn
 
 def extract(opts):
     """
@@ -40,8 +42,30 @@ def extract(opts):
         return False
 
     log('ensure target revision is up-to-date in work tree')
-    if not GIT.execute([git_dir, work_tree, 'reset', revision], cwd=work_dir):
-        err('unable to checkout revision')
-        return False
+    origin_revision = 'origin/{}'.format(revision)
+    output = []
+    if GIT.execute([git_dir, 'rev-parse', '--quiet', '--verify',
+            origin_revision], quiet=True, capture=output):
+        remote_revision = ''.join(output)
+
+        output = []
+        GIT.execute([git_dir, 'rev-parse', '--quiet', '--verify', 'HEAD'],
+            quiet=True, capture=output)
+        local_revision = ''.join(output)
+
+        debug('remote revision: {}', remote_revision)
+        debug('local revision: {}', local_revision)
+
+        if local_revision != remote_revision:
+            warn('diverged revision detected; attempting to correct...')
+            if not GIT.execute(
+                    [
+                        git_dir,
+                        work_tree,
+                        'reset',
+                        origin_revision,
+                    ], cwd=work_dir):
+                err('unable to checkout revision')
+                return False
 
     return True
