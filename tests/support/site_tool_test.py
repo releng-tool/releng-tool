4f532961a65a3c1339c20e846140fcbb35e8af26
James Knight <james.d.knight@live.com>
2021-08-15 21:58:56 -0400

    tests: provide a git-site tools tests
    Provide a series of unit tests to help validate support for the use of
    the Git tool in releng-tool. This commit adds a `TestSiteToolBase` base
    class to help support site-specific tool verification. The first attempt
    to help improving tool validation is done with the verification of a
    series of Git capabilities -- specifically, the ability to fetch and
    extract repositories of different revisions with various configurations.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ tests/support/site_tool_test.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 releng-tool
+
+from releng_tool.defs import GlobalAction
+from releng_tool.util.io import generate_temp_dir
+from releng_tool.util.io import interim_working_dir
+from releng_tool.util.io import path_remove
+from tests import prepare_testenv
+import os
+import unittest
+
+PKG_NAME = 'test'
+PKG_DEFINITION = os.path.join('package', PKG_NAME, PKG_NAME)
+PKG_CFG_PREFIX = 'TEST_'
+DEFAULT_TEMPLATE = 'site-tool'
+
+class TestSiteToolBase(unittest.TestCase):
+    """
+    site tool testing unit test base
+
+    Provides a base class for unit testing site-specific tools supported by
+    releng-tool. Using this base helps prepare an interim repository directory,
+    as well as a releng-tool engine/environment with a package configuration
+    configured to use the interim repository directory as a site location.
+
+    A unit test can build/manipulate a repository's contents, modify a test
+    package's configuration and invoke a prepared releng-tool engine to help
+    sanity check that releng-tool handles various package options and repository
+    states.
+    """
+
+    def run(self, result=None):
+        """
+        run the test
+
+        Run the test, collecting the result into the TestResult object passed as
+        result. See `unittest.TestCase.run()` for more details.
+
+        Args:
+            result (optional): the test result to populate
+        """
+
+        with generate_temp_dir() as repo_dir, interim_working_dir(repo_dir):
+            self.repo_dir = repo_dir
+            self.prepare_repo_dir(repo_dir)
+
+            with prepare_testenv(template=self.tool_template()) as engine:
+                opts = engine.opts
+
+                # by default, only perform up to the extraction stage
+                opts.gbl_action = GlobalAction.EXTRACT
+
+                self.cache_dir = os.path.join(opts.cache_dir, PKG_NAME)
+                self.defconfig = os.path.join(opts.root_dir, PKG_DEFINITION)
+                self.engine = engine
+
+                self.defconfig_add('SITE', self.repo_dir)
+                self.prepare_defconfig(self.defconfig)
+
+                super(TestSiteToolBase, self).run(result)
+
+    def prepare_defconfig(self, defconfig):
+        """
+        hook invoked to prepare a package's definition
+
+        Method invoked to allow an implementer help prepare a package's
+        definition file to be used by a releng-tool engine.
+
+        Args:
+            defconfig: the definition file
+        """
+        pass
+
+    def prepare_repo_dir(self, repo_dir):
+        """
+        hook invoked to prepare a repository
+
+        Method invoked to allow an implementer help prepare a mocked
+        repository's directory contents.
+
+        Note that this call is invoked where the working directory is configured
+        to the same path provided by `repo_dir`.
+
+        Args:
+            repo_dir: the repository directory
+        """
+        pass
+
+    def tool_template(self):
+        """
+        the configuration template to use
+
+        Returns the configuration template to be used when preparing a test
+        environment for a unit test. This call is to be overridden by
+        implementations using TestSiteToolBase.
+
+        Returns:
+            the template name; `None` if no template is to be used
+        """
+        return DEFAULT_TEMPLATE
+
+    def cleanup_outdir(self):
+        """
+        cleanup the output directory
+
+        Provides a unit test the ability to quickly cleanup the output directory
+        of the configured output directory of a test.
+        """
+        path_remove(self.engine.opts.out_dir)
+
+    def defconfig_add(self, key, value):
+        """
+        inject a configuration into the test package's definition
+
+        This method will inject a configuration entry named by `key` and append
+        it to the running test's package definition. The representation of the
+        provided value will be used as an assignment for the configuration key.
+
+        Args:
+            key: the key to use for the configuration entry
+            value: the value to use for the configuration entry
+        """
+        with open(self.defconfig, 'a') as file_def:
+            file_def.write('{}{} = {}\n'.format(
+                PKG_CFG_PREFIX, key, repr(value)))
+
+    def defconfig_dump(self):
+        """
+        dump the test package's definition contents to standard out
+
+        When requested, the test package's definition file will be read and
+        dumped to the configured standard output stream.
+        """
+        with open(self.defconfig, mode='r', encoding='utf_8') as f:
+            content = f.readlines()
+        print('-------------------------------')
+        print(''.join(content).strip())
+        print('-------------------------------')
---
+++ tests/tool-tests/test_tool_git.py
@@ -0,0 +1,300 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 releng-tool
+
+from releng_tool.defs import GlobalAction
+from releng_tool.util.io import execute
+from releng_tool.util.io import interim_working_dir
+from tests.support.site_tool_test import TestSiteToolBase
+
+DEFAULT_BRANCH = 'test'
+
+class TestToolGit(TestSiteToolBase):
+    def prepare_defconfig(self, defconfig):
+        self.defconfig_add('VCS_TYPE', 'git')
+
+    def prepare_repo_dir(self, repo_dir):
+        self._git_repo('init', repo_dir, '--initial-branch=' + DEFAULT_BRANCH)
+        self._create_commit('initial commit')
+
+    def test_tool_git_basic_branch(self):
+        self.defconfig_add('VERSION', DEFAULT_BRANCH)
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+    def test_tool_git_basic_commit(self):
+        hash = self._create_commit()
+        self.defconfig_add('VERSION', hash)
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+    def test_tool_git_basic_tag(self):
+        tag = self._create_tag('my-tag')
+        self.defconfig_add('VERSION', tag)
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+    def test_tool_git_changing_revision(self):
+        # verify pulling from an original tag
+        first_hash = self._create_commit('first commit')
+        first_tag = self._create_tag('first')
+
+        self.defconfig_add('VERSION', first_tag)
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        current_hash = self._git_cache('rev-parse', 'HEAD')
+        self.assertEqual(current_hash, first_hash)
+
+        # cleanup
+        self.cleanup_outdir()
+
+        # create a new remote revision, update the target and see if it uses the
+        # newer tag
+        second_hash = self._create_commit('second commit')
+        second_tag = self._create_tag('second')
+
+        self.defconfig_add('VERSION', second_tag)
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        # cleanup
+        self.cleanup_outdir()
+
+        # update the package definition to use the older tag and see if we can
+        # still load the older tag properly
+        current_hash = self._git_cache('rev-parse', 'HEAD')
+        self.assertEqual(current_hash, second_hash)
+
+        self.defconfig_add('VERSION', first_tag)
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        current_hash = self._git_cache('rev-parse', 'HEAD')
+        self.assertEqual(current_hash, first_hash)
+
+    def test_tool_git_custom_options(self):
+        MY_CUSTOM_CFG = 'tmp.example'
+        MY_CUSTOM_CFG_VALUE = 'my-value'
+
+        self.defconfig_add('GIT_CONFIG', {
+            MY_CUSTOM_CFG: MY_CUSTOM_CFG_VALUE,
+        })
+        self.defconfig_add('VERSION', DEFAULT_BRANCH)
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        fetched_cfg = self._git_cache('config', '--get', MY_CUSTOM_CFG)
+        self.assertEqual(fetched_cfg, MY_CUSTOM_CFG_VALUE)
+
+    def test_tool_git_custom_reference(self):
+        # create a commit on a new custom reference
+        TMP_BRANCH = 'tmp'
+        self._git_repo('checkout', '-b', TMP_BRANCH)
+        hash = self._create_commit()
+        self._git_repo('update-ref', 'refs/mycustomref/test', hash)
+        self._git_repo('checkout', DEFAULT_BRANCH)
+        self._git_repo('branch', '-D', TMP_BRANCH)
+
+        # first fetch attempt should fail since the custom reference will not be
+        # fetched by default
+        self.defconfig_add('VERSION', hash)
+        rv = self.engine.run()
+        self.assertFalse(rv)
+
+        # add the new custom reference to the configuration and re-fetch
+        self.defconfig_add('GIT_REFSPECS', ['mycustomref/*'])
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+    def test_tool_git_depth_shallow_default(self):
+        self._create_commit()
+        hash = self._create_commit()
+        self.defconfig_add('VERSION', hash)
+
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        rev_count = self._git_cache('rev-list', '--count', 'HEAD')
+        self.assertEqual(int(rev_count), 1)
+
+    def test_tool_git_depth_shallow_disabled_config(self):
+        self._create_commit()
+        hash = self._create_commit()
+        self.defconfig_add('GIT_DEPTH', 0)
+        self.defconfig_add('VERSION', hash)
+
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        rev_count = self._git_cache('rev-list', '--count', 'HEAD')
+        self.assertNotEqual(int(rev_count), 1)
+
+    def test_tool_git_depth_shallow_disabled_quirk(self):
+        self.engine.opts.quirks.append('releng.git.no_depth')
+
+        self._create_commit()
+        hash = self._create_commit()
+        self.defconfig_add('VERSION', hash)
+
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        rev_count = self._git_cache('rev-list', '--count', 'HEAD')
+        self.assertNotEqual(int(rev_count), 1)
+
+    def test_tool_git_depth_unshallow_initial(self):
+        hash = self._create_commit()
+        self._create_commit()
+        self.defconfig_add('VERSION', hash)
+
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        rev_count = self._git_cache('rev-list', '--count', 'HEAD')
+        self.assertNotEqual(int(rev_count), 1)
+
+    def test_tool_git_depth_unshallow_updated(self):
+        self._create_commit()
+        commit_a = self._create_commit()
+        commit_b = self._create_commit()
+        self.defconfig_add('VERSION', commit_b)
+
+        # the engine run should fetch a single commit
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        current_hash = self._git_cache('rev-parse', 'HEAD')
+        self.assertEqual(current_hash, commit_b)
+        rev_count = self._git_cache('rev-list', '--count', 'HEAD')
+        self.assertEqual(int(rev_count), 1)
+
+        # cleanup
+        self.cleanup_outdir()
+
+        # update to a older commit, check if the engine fetch will unshallow the
+        # currently shallow state cache
+        self.defconfig_add('VERSION', commit_a)
+
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        current_hash = self._git_cache('rev-parse', 'HEAD')
+        self.assertEqual(current_hash, commit_a)
+        rev_count = self._git_cache('rev-list', '--count', 'HEAD')
+        self.assertNotEqual(int(rev_count), 1)
+
+    def test_tool_git_diverged_branch(self):
+        # create a new branch with a commit
+        TARGET_BRANCH = 'target'
+        self._git_repo('checkout', '-b', TARGET_BRANCH)
+        first_hash = self._create_commit('first')
+
+        # fetch the target
+        self.defconfig_add('VERSION', TARGET_BRANCH)
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        current_hash = self._git_cache('rev-parse', 'HEAD')
+        self.assertEqual(current_hash, first_hash)
+
+        # cleanup
+        self.cleanup_outdir()
+
+        # destroy the old branch and build a new one with the same name
+        self._git_repo('checkout', DEFAULT_BRANCH)
+        self._git_repo('branch', '-D', TARGET_BRANCH)
+        self._git_repo('checkout', '-b', TARGET_BRANCH)
+        second_hash = self._create_commit('second')
+
+        # force a fetch (which should update the cache)
+        self.engine.opts.gbl_action = GlobalAction.FETCH
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        # fetch/extract the new target
+        self.engine.opts.gbl_action = GlobalAction.EXTRACT
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        current_hash = self._git_cache('rev-parse', 'HEAD')
+        self.assertEqual(current_hash, second_hash)
+
+    def test_tool_git_remote_tag_change(self):
+        tag_name = 'my-tag'
+
+        first_hash = self._create_commit('first')
+        self._create_tag(tag_name)
+        self.defconfig_add('VERSION', tag_name)
+
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        current_hash = self._git_cache('rev-parse', 'HEAD')
+        self.assertEqual(current_hash, first_hash)
+
+        # cleanup
+        self.cleanup_outdir()
+
+        # re-invoke with a new tag on a remote should not change cache state,
+        # as we should not have fetched
+        self._delete_tag(tag_name)
+        second_hash = self._create_commit('second')
+        self._create_tag(tag_name)
+
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        current_hash = self._git_cache('rev-parse', 'HEAD')
+        self.assertEqual(current_hash, first_hash)
+
+        # cleanup
+        self.cleanup_outdir()
+
+        # force a fetch (which should update the cache)
+        self.engine.opts.gbl_action = GlobalAction.FETCH
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        # re-invoke extraction stage to see if it uses the newer tagged commit
+        self.engine.opts.gbl_action = GlobalAction.EXTRACT
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        current_hash = self._git_cache('rev-parse', 'HEAD')
+        self.assertEqual(current_hash, second_hash)
+
+    def test_tool_git_unknown_branch_tag(self):
+        self.defconfig_add('VERSION', 'unknown')
+        rv = self.engine.run()
+        self.assertFalse(rv)
+
+    def test_tool_git_unknown_hash(self):
+        self.defconfig_add('VERSION',
+            'da39a3ee5e6b4b0d3255bfef95601890afd80709')
+        rv = self.engine.run()
+        self.assertFalse(rv)
+
+    def _git(self, workdir, *args):
+        with interim_working_dir(workdir):
+            out = []
+            if not execute(['git'] + list(args), capture=out, critical=False):
+                print('\n'.join(out))
+                assert False, 'failed to issue git command'
+            return '\n'.join(out)
+
+    def _git_cache(self, *args):
+        return self._git(self.cache_dir, *args)
+
+    def _git_repo(self, *args):
+        return self._git(self.repo_dir, *args)
+
+    def _create_commit(self, msg='test'):
+        self._git_repo('commit', '--allow-empty', '-m', msg)
+        return self._git_repo('rev-parse', 'HEAD')
+
+    def _create_tag(self, tag):
+        self._git_repo('tag', tag)
+        return tag
+
+    def _delete_tag(self, tag):
+        self._git_repo('tag', '-d', tag)
