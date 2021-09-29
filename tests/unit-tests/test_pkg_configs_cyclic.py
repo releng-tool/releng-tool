c29a5677fcbc679cd24ba06278899f59e1f99258
James Knight <james.d.knight@live.com>
2021-09-28 22:57:17 -0400

    tests: adding cyclic package detection checks
    Adding a series of unit tests to validate the detection of cyclic
    packages when loading through the package manager.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ tests/unit-tests/test_pkg_configs_cyclic.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 releng-tool
+
+from releng_tool.packages.exceptions import RelengToolCyclicPackageDependency
+from tests import prepare_testenv
+from tests.support.pkg_config_test import TestPkgConfigsBase
+
+class TestPkgConfigsCyclic(TestPkgConfigsBase):
+    def test_pkgconfig_cyclic_explicit_loading(self):
+        pkg_names = [
+            'test-a',
+            'test-b',
+            'test-c',
+        ]
+
+        with prepare_testenv(template='cyclic') as engine:
+            with self.assertRaises(RelengToolCyclicPackageDependency):
+                engine.pkgman.load(pkg_names)
+
+    def test_pkgconfig_cyclic_implicit_loading(self):
+        pkg_names = [
+            'test-b',
+        ]
+
+        with prepare_testenv(template='cyclic') as engine:
+            with self.assertRaises(RelengToolCyclicPackageDependency):
+                engine.pkgman.load(pkg_names)
+
+    def test_pkgconfig_cyclic_self(self):
+        pkg_names = [
+            'test-d',
+        ]
+
+        with prepare_testenv(template='cyclic') as engine:
+            with self.assertRaises(RelengToolCyclicPackageDependency):
+                engine.pkgman.load(pkg_names)
