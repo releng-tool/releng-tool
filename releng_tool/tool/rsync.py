9a03c0048d744bdf7544ccec2f37b71b03fcf086
James Knight <james.d.knight@live.com>
2021-12-31 14:43:21 -0500

    tool: simplify rsync tool
    The initial implementation of the rsync tool entry was created based off
    the scp tool; however, using distutil is not required when detecting
    rsync (i.e. a simple version command line check works for rsync).
    Minimizing this implementation to use the existing tool base class
    implementation.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- releng_tool/tool/rsync.py
+++ releng_tool/tool/rsync.py
@@ -2,8 +2,6 @@
 # Copyright 2021 releng-tool
 
 from releng_tool.tool import RelengTool
-from releng_tool.util.log import debug
-import distutils.spawn
 
 #: executable used to run rsync commands
 RSYNC_COMMAND = 'rsync'
@@ -14,33 +12,5 @@ RSYNC_SANITIZE_ENV_KEYS = [
     'RSYNC_PARTIAL_DIR',
 ]
 
-class RsyncTool(RelengTool):
-    """
-    rsync host tool
-
-    Provides addition helper methods for rsync-based tool interaction.
-    """
-
-    def exists(self):
-        """
-        return whether or not the host tool exists
-
-        Returns whether or not the tool is available on the host for use.
-
-        Returns:
-            ``True``, if the tool exists; ``False`` otherwise
-        """
-        if self.tool in RelengTool.detected:
-            return RelengTool.detected[self.tool]
-
-        if distutils.spawn.find_executable(self.tool):
-            debug('{} tool is detected on this system', self.tool)
-            RelengTool.detected[self.tool] = True
-        else:
-            debug('{} tool is not detected on this system', self.tool)
-            RelengTool.detected[self.tool] = False
-
-        return RelengTool.detected[self.tool]
-
 #: rsync host tool helper
-RSYNC = RsyncTool(RSYNC_COMMAND, env_sanitize=RSYNC_SANITIZE_ENV_KEYS)
+RSYNC = RelengTool(RSYNC_COMMAND, env_sanitize=RSYNC_SANITIZE_ENV_KEYS)
