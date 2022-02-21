4b6d983f092e46abc4e983271dfa8e9b145f54c8
James Knight <james.d.knight@live.com>
2022-02-21 14:58:10 -0500

    tests: adding tests for require-version function
    Adding a series of sanity check tests for the support `require_version`
    function.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ tests/unit-tests/test_support.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 releng-tool
+
+from releng_tool import __version__ as releng_version
+from releng_tool.support import require_version
+import unittest
+
+
+class TestSupport(unittest.TestCase):
+    def test_support_require_version(self):
+        # existing version should report has valid
+        result = require_version(releng_version)
+        self.assertTrue(result)
+
+        # an old version should report has valid
+        result = require_version('0.0')
+        self.assertTrue(result)
+
+        # fail version check on a future version which does not exist
+        result = require_version('999999', critical=False)
+        self.assertFalse(result)
+
+        # fail version check on a future version which does not exist (critical)
+        with self.assertRaises(SystemExit):
+            require_version('999999')
