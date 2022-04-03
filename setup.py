0f34b92355f75d2263b7302581da30b9010f9278
James Knight <james.d.knight@live.com>
2022-04-03 14:57:42 -0400

    setup: move meta fields from setup.py to setup.cfg
    Moving a series of meta fields from the `setup.py` file into
    `setup.cfg`.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ from setuptools import find_packages
 from setuptools import setup
 import os
 
-def read(name):
-    return open(os.path.join(os.path.dirname(__file__), name)).read()
-
 # remove extra resources not removed by the default clean operation
 class ExtendedClean(clean):
     def run(self):
@@ -28,30 +25,9 @@ class ExtendedClean(clean):
                 dir_util.remove_tree(extra, dry_run=self.dry_run)
 
 setup(
-    author='releng-tool',
-    author_email='releng@releng.io',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Build Tools',
-    ],
     cmdclass={
         'clean': ExtendedClean,
     },
-    description='release engineering utility tool',
     entry_points={
         'console_scripts': [
             'releng-tool = releng_tool.__main__:main',
@@ -62,13 +38,8 @@ setup(
             'matplotlib',
         ],
     },
-    license='BSD-2-Clause',
-    long_description=read('README.rst'),
-    name='releng-tool',
     packages=find_packages(exclude=["tests*"]),
-    platforms='any',
     test_suite='tests',
-    url='https://releng.io',
     version='0.12.0.dev0',
     zip_safe=False,
 )
