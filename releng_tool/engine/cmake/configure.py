d7a9c0a0ec4b581313357e178887ceea670952ef
James Knight <james.d.knight@live.com>
2022-04-03 13:46:00 -0400

    copyright bump
    Signed-off-by: James Knight <james.d.knight@live.com>

--- releng_tool/engine/cmake/configure.py
+++ releng_tool/engine/cmake/configure.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018-2021 releng-tool
+# Copyright 2018-2022 releng-tool
 
 from releng_tool.defs import PackageInstallType
 from releng_tool.tool.cmake import CMAKE
