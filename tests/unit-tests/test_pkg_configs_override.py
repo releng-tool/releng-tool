cfd62c17d1da77198f7967eaade5d47857494481
James Knight <james.d.knight@live.com>
2022-04-03 13:56:02 -0400

    tests: verify ability to override package variables
    Adding a test to verify the user's ability to inject CLI overrides.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ tests/unit-tests/test_pkg_configs_override.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 releng-tool
+
+from releng_tool.opts import RelengEngineOptions
+from releng_tool.packages.manager import RelengPackageManager
+from releng_tool.registry import RelengRegistry
+from tests.support.pkg_config_test import TestPkgConfigsBase
+
+class TestPkgConfigsOverride(TestPkgConfigsBase):
+    def test_pkgconfig_override_configured(self):
+        # provide an override for the package's revision
+        opts = RelengEngineOptions()
+        opts.injected_kv = {
+            'TEST_REVISION': 'overridden',
+        }
+
+        registry = RelengRegistry()
+        manager = RelengPackageManager(opts, registry)
+
+        pkg, _, _ = self.LOAD('override-check', manager=manager)
+        self.assertEqual(pkg.revision, 'overridden')
+
+    def test_pkgconfig_override_notconfigured(self):
+        pkg, _, _ = self.LOAD('override-check')
+        self.assertEqual(pkg.revision, 'no-override')
