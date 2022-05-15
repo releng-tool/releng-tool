10006e704613a592b254a3ae1181c86b610c7c4f
James Knight <james.d.knight@live.com>
2022-05-15 11:26:10 -0400

    tests: rename engine run environment configuration validation
    Renaming a test file to better indicate it's used for validating
    input-based environment variable; so prepare for upcoming releng-tool
    engine-set environment test checks.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- tests/unit-tests/test_engine_run_environ_config.py
+++ tests/unit-tests/test_engine_run_environ_config.py
@@ -1,22 +1,14 @@
 # -*- coding: utf-8 -*-
-# Copyright 2021 releng-tool
+# Copyright 2021-2022 releng-tool
 
 from tests import prepare_testenv
 from tests import prepare_workdir
+from tests.support.env_test import EnvironmentTestCase
 import os
-import unittest
 
-class TestEngineRunEnviron(unittest.TestCase):
 
-    def run(self, result=None):
-        old_env = dict(os.environ)
-        try:
-            super(TestEngineRunEnviron, self).run(result)
-        finally:
-            os.environ.clear()
-            os.environ.update(old_env)
-
-    def test_engine_run_environ_assets_dir(self):
+class TestEngineRunEnvironConfig(EnvironmentTestCase):
+    def test_engine_run_environ_cfg_assets_dir(self):
         with prepare_workdir() as assets_dir:
             os.environ['RELENG_ASSETS_DIR'] = assets_dir
 
@@ -26,14 +18,14 @@ class TestEngineRunEnviron(unittest.TestCase):
                 self.assertEqual(engine.opts.cache_dir, expected_cache_dir)
                 self.assertEqual(engine.opts.dl_dir, expected_dl_dir)
 
-    def test_engine_run_environ_cache_dir(self):
+    def test_engine_run_environ_cfg_cache_dir(self):
         with prepare_workdir() as cache_dir:
             os.environ['RELENG_CACHE_DIR'] = cache_dir
 
             with prepare_testenv() as engine:
                 self.assertEqual(engine.opts.cache_dir, cache_dir)
 
-    def test_engine_run_environ_dl_dir(self):
+    def test_engine_run_environ_cfg_dl_dir(self):
         with prepare_workdir() as dl_dir:
             os.environ['RELENG_DL_DIR'] = dl_dir
 
