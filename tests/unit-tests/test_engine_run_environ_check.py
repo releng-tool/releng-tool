726f4568b6d12148cdd36e2554e23f491d490c13
James Knight <james.d.knight@live.com>
2022-05-15 11:27:24 -0400

    tests: validate optional env/script variables
    Verify that all optional environment/script variables are properly set
    to `None` (for script variable) or not set (for environment variables).
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- tests/unit-tests/test_engine_run_environ_check.py
+++ tests/unit-tests/test_engine_run_environ_check.py
@@ -7,6 +7,28 @@ import os
 
 
 class TestEngineRunEnvironCheck(EnvironmentTestCase):
+    def test_engine_run_environ_check_defaults(self):
+        rv = run_testenv(template='env-defaults')
+        self.assertTrue(rv)
+
+        optional_env_vars = [
+            'RELENG_CLEAN',
+            'RELENG_DEBUG',
+            'RELENG_DEVMODE',
+            'RELENG_DISTCLEAN',
+            'RELENG_FORCE',
+            'RELENG_LOCALSRCS',
+            'RELENG_MRPROPER',
+            'RELENG_REBUILD',
+            'RELENG_RECONFIGURE',
+            'RELENG_REINSTALL',
+            'RELENG_TARGET_PKG',
+            'RELENG_VERBOSE',
+        ]
+
+        for var in optional_env_vars:
+            self.assertFalse(var in os.environ, var)
+
     def test_engine_run_environ_check_target_pkg(self):
         config = {
             'action': 'minimal-fetch',
