9f0e8322b07915eb81f25287de6f82dfaa62f84d
James Knight <james.d.knight@live.com>
2022-06-18 10:58:13 -0400

    pyproject.toml: introduction
    Introduce the use of a `pyproject.toml` file. This is to provide some
    initial steps to move away from the usage of a `setup.py` file.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- setup.py
+++ setup.py
@@ -2,44 +2,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018-2022 releng-tool
 
-from distutils.command.clean import clean
-from distutils import dir_util
-from setuptools import find_packages
-from setuptools import setup
-import os
+import setuptools
 
-# remove extra resources not removed by the default clean operation
-class ExtendedClean(clean):
-    def run(self):
-        clean.run(self)
-
-        if not self.all:
-            return
-
-        extras = [
-            'dist',
-            'releng_tool.egg-info',
-        ]
-        for extra in extras:
-            if os.path.exists(extra):
-                dir_util.remove_tree(extra, dry_run=self.dry_run)
-
-setup(
-    cmdclass={
-        'clean': ExtendedClean,
-    },
-    entry_points={
-        'console_scripts': [
-            'releng-tool = releng_tool.__main__:main',
-        ],
-    },
-    extras_require={
-        'statistics': [
-            'matplotlib',
-        ],
-    },
-    packages=find_packages(exclude=["tests*"]),
-    test_suite='tests',
-    version='0.13.0.dev0',
-    zip_safe=False,
-)
+if __name__ == '__main__':
+    setuptools.setup()
