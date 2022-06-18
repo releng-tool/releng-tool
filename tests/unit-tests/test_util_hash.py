8e32d704446bb23141fac2352fc754c851ca24b7
James Knight <james.d.knight@live.com>
2022-06-18 11:04:22 -0400

    tests: expand hash testing to handle new keylen option
    Tweak the hashing tests to expand testing on key-length required hash
    algorithms.
    
    Tests have also been expanded to verify the usage of newer FIPS
    algorithm types, depending on the interpreter used.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- tests/unit-tests/test_util_hash.py
+++ tests/unit-tests/test_util_hash.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 releng-tool
+# Copyright 2018-2022 releng-tool
 
 from releng_tool.util.hash import HashResult
 from releng_tool.util.hash import BadFileHashLoadError
@@ -7,10 +7,13 @@ from releng_tool.util.hash import BadFormatHashLoadError
 from releng_tool.util.hash import load as load_hashes
 from releng_tool.util.hash import verify as verify_hashes
 import os
+import sys
 import unittest
+
 
 ASSETS_DIR = 'assets'
 SAMPLES_DIR = 'hash-samples'
+
 
 class TestUtilHash(unittest.TestCase):
     @classmethod
@@ -57,6 +60,15 @@ class TestUtilHash(unittest.TestCase):
         file = os.path.join(samples, 'valid-hash-sample-c')
         data = load_hashes(file)
         self.assertEqual(data, [
+            ('blake2b',
+                '2de3fb97c83a6c959f7ca1f6dffd7a2a4533a54b0542a49069ff15d45c928f'
+                'f7747488c4b697a82a6bc133d4b0b99ca23d97d0e244347a8f3966740afbc5'
+                'bb24',
+                'sample-asset'),
+            ('blake2s',
+                '223725c4a88bc1c93f7d073ce37085e51fc8150137a3607531ea3025df9b65'
+                'eb',
+                'sample-asset'),
             ('md5',
                 '37a460dd2f87f3c04a4a85b38d922eb1',
                 'sample-asset'),
@@ -74,10 +86,35 @@ class TestUtilHash(unittest.TestCase):
                 '60d4b57034872b8617bb1a0a0e076d0a97a6d44485c5e013e9156b8f51346d'
                 '58b36ea1be51c57c23600aed1fb301e07f',
                 'sample-asset'),
+            ('sha3_224',
+                '3aff99f4000c02453dad244565c74bc764b27b77aafda64e88fb1765',
+                'sample-asset'),
+            ('sha3_256',
+                '933114c8a52661f9a8e6a3e659fc307f3454cff30da97035a556d095fd9caa'
+                '23',
+                'sample-asset'),
+            ('sha3_384',
+                '4d206cf4bae2d4465a43ffab5c76bb2e8e7dfd593dd4793f64eb5a81431877'
+                '089851c9f4de6ec3a4244154d6dc3e3e6c',
+                'sample-asset'),
+            ('sha3_512',
+                'e8bf4924c9df7604fc3e8eed08a0fe3e83d4c00ce8458106a737a3bb71f59b'
+                '370df6ffa977ac5056484c45de2a3944c13435f7af75d069c8b6143717bd79'
+                '1f0d',
+                'sample-asset'),
             ('sha512',
                 '741695d2189840162a19d9a8f8c99e7adc4621f452b43d20081a5ebb23b9d7'
                 '3e15c3ffb9b06c65621da269686da2d5991f5ac415cdd2cfcd5faf10d3aa47'
                 'd728',
+                'sample-asset'),
+            ('shake_128:32',
+                'd31029b471ea2797944b90c578d7f683caf5026aca5e02043e688066473ad9'
+                '85',
+                'sample-asset'),
+            ('shake_256:64',
+                '039bf1dc6f102006eacd32fdd4bea405303bc09d1739ec1c4e52b956fd282a'
+                '7d50e8580232e653cd191605226a3c06f178e3b6eafa6b7a6e7944e2cbbb2e'
+                '4329',
                 'sample-asset'),
         ])
 
@@ -90,17 +127,27 @@ class TestUtilHash(unittest.TestCase):
         samples = self.samples_dir
         target_sample = os.path.join(samples, 'sample-asset')
 
+        # since Python 3.6, blake/sha3/shake are available
+        if sys.version_info >= (3, 6):
+            verify_success = 'verify-success-since-36'
+        else:
+            verify_success = 'verify-success-pre-36'
+
         # archive-specific check
         file = os.path.join(samples, 'verify-missing-archive')
         result = verify_hashes(file, target_sample, quiet=True)
         self.assertEqual(result, HashResult.MISSING_ARCHIVE)
 
-        file = os.path.join(samples, 'verify-success')
-        result = verify_hashes(file, target_sample, quiet=True)
+        file = os.path.join(samples, verify_success)
+        result = verify_hashes(file, target_sample)
         self.assertEqual(result, HashResult.VERIFIED)
 
         # directory-specific check
-        file = os.path.join(samples, 'verify-bad-format')
+        file = os.path.join(samples, 'verify-bad-format-keylen')
+        result = verify_hashes(file, samples, quiet=True)
+        self.assertEqual(result, HashResult.BAD_FORMAT)
+
+        file = os.path.join(samples, 'verify-bad-format-missing-file')
         result = verify_hashes(file, samples, quiet=True)
         self.assertEqual(result, HashResult.BAD_FORMAT)
 
@@ -120,7 +167,7 @@ class TestUtilHash(unittest.TestCase):
         result = verify_hashes(file, samples, quiet=True)
         self.assertEqual(result, HashResult.MISSING_LISTED)
 
-        file = os.path.join(samples, 'verify-success')
+        file = os.path.join(samples, verify_success)
         result = verify_hashes(file, samples, quiet=True)
         self.assertEqual(result, HashResult.VERIFIED)
 
@@ -134,6 +181,6 @@ class TestUtilHash(unittest.TestCase):
 
         # excluded/empty
         exclude = ['sample-asset']
-        file = os.path.join(samples, 'verify-success')
-        result = verify_hashes(file, target_sample, exclude=exclude, quiet=True)
+        file = os.path.join(samples, verify_success)
+        result = verify_hashes(file, target_sample, exclude=exclude)
         self.assertEqual(result, HashResult.EMPTY)
