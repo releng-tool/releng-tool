ae1668fb6ab873145e171d4b61074b4b671a8441
James Knight <james.d.knight@live.com>
2022-08-01 09:01:13 -0400

    tests: add expansion checks in package definitions
    Verify that desired fields in a package definition are expandable.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ tests/unit-tests/test_pkg_configs_expand.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 releng-tool
+
+from releng_tool.opts import RelengEngineOptions
+from releng_tool.packages.manager import RelengPackageManager
+from releng_tool.registry import RelengRegistry
+from tests.support.pkg_config_test import TestPkgConfigsBase
+
+
+class TestPkgConfigsExpand(TestPkgConfigsBase):
+    def test_pkgconfig_expand_check01(self):
+        pkg, _, _ = self.LOAD('expand')
+        self.assertEqual(pkg.version, 'version')
+        self.assertEqual(pkg.revision, 'revision')
+        self.assertEqual(pkg.site, 'site-revision')
+
+    def test_pkgconfig_expand_check02(self):
+        opts = RelengEngineOptions()
+        opts.devmode = 'version-lts'
+
+        registry = RelengRegistry()
+        manager = RelengPackageManager(opts, registry)
+
+        pkg, _, _ = self.LOAD('expand', manager=manager)
+        self.assertEqual(pkg.version, 'version-lts')
+        self.assertEqual(pkg.revision, 'version-lts')
+        self.assertEqual(pkg.site, 'site-version-lts-archive')
