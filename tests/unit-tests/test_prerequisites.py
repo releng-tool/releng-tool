f6fe5b55308cdd9d37eaa46678f38644604e682a
James Knight <james.d.knight@live.com>
2022-08-06 14:15:29 -0400

    tests: validate prerequisite exclusions
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ tests/unit-tests/test_prerequisites.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 releng-tool
+
+from releng_tool.prerequisites import RelengPrerequisites
+import unittest
+
+
+class TestPrerequisites(unittest.TestCase):
+    def test_prerequisites_exclude(self):
+        prerequisites = RelengPrerequisites([], ['misc-command'])
+        check = prerequisites.check(exclude=['misc-command'])
+        self.assertTrue(check)
+
+    def test_prerequisites_exist(self):
+        # python should exist; since we rely on python
+        prerequisites = RelengPrerequisites([], ['python'])
+        check = prerequisites.check()
+        self.assertTrue(check)
+
+    def test_prerequisites_missing(self):
+        prerequisites = RelengPrerequisites([], ['unknown-command-releng'])
+        check = prerequisites.check()
+        self.assertFalse(check)
