ce0d2e9853cf70a1e67c202b99913b09aa4a49f7
James Knight <james.d.knight@live.com>
2022-08-07 01:53:40 -0400

    copyright bump
    Signed-off-by: James Knight <james.d.knight@live.com>

--- releng_tool/tool/__init__.py
+++ releng_tool/tool/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018-2021 releng-tool
+# Copyright 2018-2022 releng-tool
 
 from releng_tool.util.io import _execute
 from releng_tool.util.io import execute
--- tests/unit-tests/test_pkg_configs_type.py
+++ tests/unit-tests/test_pkg_configs_type.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2021 releng-tool
+# Copyright 2021-2022 releng-tool
 
 from releng_tool.defs import PackageType
 from releng_tool.packages.exceptions import RelengToolInvalidPackageKeyValue
