a424f6aff4406d28c1792411eddbfcec0e725dc9
James Knight <james.d.knight@live.com>
2023-01-21 21:04:55 -0500

    permit `.releng` extension for releng-tool files
    Originally extension-less, the `.py` extension was permitted for
    releng-tool configuration files. Now, introducing a `.releng` variant
    to test out another means of identification.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- releng_tool/util/io.py
+++ releng_tool/util/io.py
@@ -35,6 +35,12 @@ MULTIPART_EXTENSIONS = [
     'tar.z',
 ]
 
+# file extensions permitted for releng-tool scripts/definitions
+RELENG_TOOL_EXTENSIONS = [
+    '.releng',
+    '.py',
+]
+
 
 class FailedToPrepareBaseDirectoryError(Exception):
     """
@@ -632,14 +638,14 @@ def opt_file(file):
 
     Various user-defined scripts by default do not have an extension. For
     example, a project's releng-tool script is defined by a file named `releng`;
-    however, select users may wish to define a `releng.py` script instead.
-    Consider, with the previous example, the file `releng` is the "standard"
-    file and the `releng.py` script is the "alternative" file. This utility call
-    will return the file path and the existence state of the returned file. If
-    the standard file does not exist but the alternative file does, this call
-    will return the alternative file. Priority is given to the standard file, so
-    if neither file exists, this call will return the provided/standard file
-    path.
+    however, select users may wish to define a `releng.releng` or `releng.py`
+    script instead. Consider, with the previous example, the file `releng` is
+    the "standard" file and the `releng.releng` or `releng.py` script is the
+    "alternative" file. This utility call will return the file path and the
+    existence state of the returned file. If the standard file does not exist
+    but the alternative file does, this call will return the alternative file.
+    Priority is given to the standard file, so if neither file exists, this
+    call will return the provided/standard file path.
 
     Args:
         file: the file to check for
@@ -650,9 +656,10 @@ def opt_file(file):
 
     exists = os.path.isfile(file)
     if not exists:
-        flex_file = file + '.py'
-        if os.path.isfile(flex_file):
-            return flex_file, True
+        for ext in RELENG_TOOL_EXTENSIONS:
+            flex_file = file + ext
+            if os.path.isfile(flex_file):
+                return flex_file, True
 
     return file, exists
 
