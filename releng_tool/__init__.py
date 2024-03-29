f8179c82e0e8e832d1fa2c158ccb8a85c5aff118
James Knight <james.d.knight@live.com>
2024-03-29 16:01:44 -0400

    releng-tool 1.2.0.dev0
    Signed-off-by: James Knight <james.d.knight@live.com>

--- releng_tool/__init__.py
+++ releng_tool/__init__.py
@@ -2,7 +2,7 @@
 # Copyright releng-tool
 # SPDX-License-Identifier: BSD-2-Clause
 
-__version__ = '1.1.0'
+__version__ = '1.2.0.dev0'
 
 # Below should contain a series of helper implementations to assist releng-tool
 # developer's wanting to explicitly import script helpers into scripts not
