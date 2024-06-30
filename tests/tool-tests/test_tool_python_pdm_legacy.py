5aef0ef5657a6ae89c1f882a3e11f48048217315
James Knight <james.d.knight@live.com>
2024-06-30 17:36:32 -0400

    tests: support new pdm backend
    Updating testing to support a PDM project that used the original
    `pdm.pep517.api` backend, as well as the newer `pdm.backend` backend.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- tests/tool-tests/test_tool_python_pdm_legacy.py
+++ tests/tool-tests/test_tool_python_pdm_legacy.py
@@ -7,17 +7,17 @@ import sys
 import unittest
 
 
-class TestToolPythonPdm(PythonSiteToolBase):
+class TestToolPythonPdmLegacy(PythonSiteToolBase):
     @classmethod
     def setUpClass(cls):
-        # pdm is only available in Python 3.8+
-        if sys.version_info < (3, 8):
+        # pdm is only available in Python 3.7+
+        if sys.version_info < (3, 7):
             raise unittest.SkipTest('skipping for unsupported interpreters')
 
     def tool_template(self):
-        return 'python-pdm'
+        return 'python-pdm-legacy'
 
-    def test_tool_python_pdm_default(self):
+    def test_tool_python_pdm_legacy_default(self):
         rv = self.engine.run()
         self.assertTrue(rv)
 
@@ -31,7 +31,7 @@ class TestToolPythonPdm(PythonSiteToolBase):
         self.assertIsNotNone(site_packages)
         self.assertPythonModuleExists(site_packages, 'hello_module')
 
-    def test_tool_python_pdm_host(self):
+    def test_tool_python_pdm_legacy_host(self):
         self.defconfig_add('INSTALL_TYPE', 'host')
 
         rv = self.engine.run()
@@ -47,7 +47,7 @@ class TestToolPythonPdm(PythonSiteToolBase):
         site_packages = self.find_site_packages(self.engine.opts.target_dir)
         self.assertIsNone(site_packages)
 
-    def test_tool_python_pdm_staging(self):
+    def test_tool_python_pdm_legacy_staging(self):
         self.defconfig_add('INSTALL_TYPE', 'staging')
 
         rv = self.engine.run()
@@ -63,7 +63,7 @@ class TestToolPythonPdm(PythonSiteToolBase):
         site_packages = self.find_site_packages(self.engine.opts.target_dir)
         self.assertIsNone(site_packages)
 
-    def test_tool_python_pdm_staging_and_target(self):
+    def test_tool_python_pdm_legacy_staging_and_target(self):
         self.defconfig_add('INSTALL_TYPE', 'staging_and_target')
 
         rv = self.engine.run()
