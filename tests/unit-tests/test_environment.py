e18fb73a6746da138b5acd16841f80fc5f6b9669
James Knight <james.d.knight@live.com>
2024-07-12 02:18:40 -0400

    tests: validate project-defined environments
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ tests/unit-tests/test_environment.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright releng-tool
+# SPDX-License-Identifier: BSD-2-Clause
+
+from releng_tool import __version__ as releng_version
+from tests import RelengToolTestCase
+from tests import prepare_testenv
+import json
+import os
+
+
+class TestEnvironment(RelengToolTestCase):
+    def test_environment_project_injection(self):
+        with prepare_testenv(template='env-inject') as engine:
+            rv = engine.run()
+            self.assertTrue(rv)
+
+            # extract environment variables
+            capture = os.path.join(engine.opts.target_dir, 'invoke-env.json')
+            self.assertTrue(os.path.exists(capture))
+
+            with open(capture, 'r') as f:
+                data = json.load(f)
+
+            # sanity check one entry
+            self.assertIn('TEST_1', data)
+            self.assertEqual(data['TEST_1'], 'A')
+
+            # and another to show more than one
+            self.assertIn('TEST_2', data)
+            self.assertEqual(data['TEST_2'], 'B')
+
+            # also confirm variable expansion works
+            self.assertIn('TEST_3', data)
+            self.assertEqual(data['TEST_3'], releng_version)
