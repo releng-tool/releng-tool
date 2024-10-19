e0f9c136991cb1cedc5e3374769b3ad257608211
James Knight <git@jdknight.me>
2024-10-19 19:19:32 -0400

    tests: adding a series of vsdevcmd checks
    Signed-off-by: James Knight <git@jdknight.me>

---
+++ tests/unit-tests/test_pkg_configs_vsdevcmd.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright releng-tool
+# SPDX-License-Identifier: BSD-2-Clause
+
+from releng_tool.packages.exceptions import RelengToolInvalidPackageKeyValue
+from tests.support.pkg_config_test import TestPkgConfigsBase
+
+
+class TestPkgConfigsVsDevCmd(TestPkgConfigsBase):
+    def test_pkgconfig_vsdevcmd_invalid(self):
+        with self.assertRaises(RelengToolInvalidPackageKeyValue):
+            self.LOAD('vsdevcmd-invalid-type')
+
+    def test_pkgconfig_vsdevcmd_missing(self):
+        pkg, _, _ = self.LOAD('missing')
+        self.assertIsNone(pkg.vsdevcmd)
+
+    def test_pkgconfig_vsdevcmd_default(self):
+        pkg, _, _ = self.LOAD('vsdevcmd-valid-default')
+        self.assertTrue(pkg.vsdevcmd)
+
+    def test_pkgconfig_vsdevcmd_version(self):
+        pkg, _, _ = self.LOAD('vsdevcmd-valid-version')
+        self.assertEqual(pkg.vsdevcmd, '[16.4,16.5)')
---
+++ tests/unit-tests/test_vsdevcmd.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Copyright releng-tool
+# SPDX-License-Identifier: BSD-2-Clause
+
+from tests import RelengToolTestCase
+from tests import prepare_testenv
+from releng_tool.tool.vswhere import VSWHERE
+import os
+import sys
+import unittest
+
+
+class TestVsDevCmd(RelengToolTestCase):
+    @classmethod
+    def setUpClass(cls):
+        if sys.platform != 'win32':
+            raise unittest.SkipTest('only win32')
+
+        if not VSWHERE.exists():
+            raise unittest.SkipTest('environment does not have vswhere')
+
+    def test_vsdevcmd_global_default(self):
+        with prepare_testenv(template='vsdevcmd-global-default') as engine:
+            rv = engine.run()
+            self.assertTrue(rv)
+
+            target_dir = engine.opts.target_dir
+
+            expected_exists = [
+                os.path.join(target_dir, 'build-success'),
+                os.path.join(target_dir, 'post-build-success'),
+            ]
+
+            for expected in expected_exists:
+                self.assertTrue(os.path.exists(expected), expected)
+
+    def test_vsdevcmd_package_default(self):
+        with prepare_testenv(template='vsdevcmd-package-default') as engine:
+            rv = engine.run()
+            self.assertTrue(rv)
+
+            target_dir = engine.opts.target_dir
+
+            expected_exists = [
+                os.path.join(target_dir, 'build-success'),
+                os.path.join(target_dir, 'post-build-failure'),
+            ]
+
+            for expected in expected_exists:
+                self.assertTrue(os.path.exists(expected), expected)
