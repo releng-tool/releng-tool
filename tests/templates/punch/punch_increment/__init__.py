c274434cb8719d292dd0f844547ed2b999948d5e
James Knight <git@jdknight.me>
2024-10-18 22:05:27 -0400

    tests: validate punch stages
    Verify that a punch-action triggers each stage for a package and the
    post-build action.
    
    Signed-off-by: James Knight <git@jdknight.me>

---
+++ tests/templates/punch/punch_increment/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import os
+
+
+def increment_key(outdir, key):
+    test_state_db = os.path.join(outdir, 'state.json')
+
+    if os.path.exists(test_state_db):
+        with open(test_state_db, 'r') as f:
+            data = json.load(f)
+    else:
+        data = {}
+
+    post_run_count = data.get(key, 0)
+    post_run_count += 1
+    data[key] = post_run_count
+
+    with open(test_state_db, 'w') as f:
+        json.dump(data, f)
---
+++ tests/unit-tests/test_engine_run_punch.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# Copyright releng-tool
+# SPDX-License-Identifier: BSD-2-Clause
+
+from releng_tool.defs import GlobalAction
+from tests import RelengToolTestCase
+from tests import prepare_testenv
+from tests import prepare_workdir
+import json
+import os
+
+
+class TestEngineRunPunch(RelengToolTestCase):
+    def test_engine_run_punch_multipass_default(self):
+        state = self._run_tripass()
+
+        # in a default state, all stages run once excpet for post-run
+        self.assertEqual(state['bootstrap'], 1)
+        self.assertEqual(state['configure'], 1)
+        self.assertEqual(state['build'], 1)
+        self.assertEqual(state['install'], 1)
+        self.assertEqual(state['post'], 1)
+        self.assertEqual(state['post-run'], 3)
+
+    def test_engine_run_punch_multipass_punched(self):
+        state = self._run_tripass(action=GlobalAction.PUNCH)
+
+        # verify each stage has been three times
+        self.assertEqual(state['bootstrap'], 3)
+        self.assertEqual(state['configure'], 3)
+        self.assertEqual(state['build'], 3)
+        self.assertEqual(state['install'], 3)
+        self.assertEqual(state['post'], 3)
+        self.assertEqual(state['post-run'], 3)
+
+    def _run_tripass(self, action=None):
+        with prepare_workdir() as output_dir:
+            config = {
+                'action': action,
+                'output_dir': output_dir,
+            }
+
+            # first pass
+            with prepare_testenv(config=config, template='punch') as engine:
+                rv = engine.run()
+                self.assertTrue(rv)
+
+            # second pass
+            with prepare_testenv(config=config, template='punch') as engine:
+                rv = engine.run()
+                self.assertTrue(rv)
+
+            # third time is a charm
+            with prepare_testenv(config=config, template='punch') as engine:
+                out_dir = engine.opts.out_dir
+
+                rv = engine.run()
+                self.assertTrue(rv)
+
+                # load state counters
+                test_state_db = os.path.join(out_dir, 'state.json')
+                self.assertTrue(os.path.exists(test_state_db))
+
+                with open(test_state_db, 'r') as f:
+                    state = json.load(f)
+
+                return state
