1e4f5f04b932e4ea128321ee86b2a2d89eb0bea8
James Knight <git@jdknight.me>
2024-10-27 13:59:55 -0400

    tests: extend verifications for def-less packages
    Signed-off-by: James Knight <git@jdknight.me>

---
+++ tests/unit-tests/test_engine_run_external_pkg.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright releng-tool
+# SPDX-License-Identifier: BSD-2-Clause
+
+from tests import RelengToolTestCase
+from tests import copy_template
+from tests import prepare_testenv
+from tests import prepare_workdir
+import os
+
+
+class TestEngineExternalPkg(RelengToolTestCase):
+    def test_engine_run_external_pkg(self):
+        with prepare_testenv(template='extern-pkg-base') as engine, \
+                prepare_workdir() as pkg_repo1, \
+                prepare_workdir() as pkg_repo2:
+
+            copy_template('extern-pkg-need1', pkg_repo1)
+            copy_template('extern-pkg-need2', pkg_repo2)
+
+            os.environ['RELENG_TOOL_TEST_NEED1'] = pkg_repo1
+            os.environ['RELENG_TOOL_TEST_NEED2'] = pkg_repo2
+
+            rv = engine.run()
+            self.assertTrue(rv)
+
+            target_dir = engine.opts.target_dir
+            flag = os.path.join(target_dir, 'invoked-bootstrap-need1')
+            self.assertTrue(os.path.exists(flag))
+
+            target_dir = engine.opts.target_dir
+            flag = os.path.join(target_dir, 'invoked-bootstrap-need2')
+            self.assertTrue(os.path.exists(flag))
--- tests/unit-tests/test_engine_run_nopkgdef.py
+++ tests/unit-tests/test_engine_run_nopkgdef.py
@@ -8,8 +8,16 @@ import os
 
 
 class TestEngineRunNoPkgDef(RelengToolTestCase):
-    def test_engine_run_nopkgdef(self):
+    def test_engine_run_nopkgdef_default(self):
         with prepare_testenv(template='package-without-pkgdef') as engine:
+            rv = engine.run()
+            self.assertTrue(rv)
+
+            file_flag = os.path.join(engine.opts.target_dir, 'build-triggered')
+            self.assertTrue(os.path.exists(file_flag))
+
+    def test_engine_run_nopkgdef_depend(self):
+        with prepare_testenv(template='package-without-pkgdef-deps') as engine:
             rv = engine.run()
             self.assertTrue(rv)
 
