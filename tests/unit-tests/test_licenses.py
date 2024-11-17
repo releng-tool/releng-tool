fbc316746342ca0f1f875ee9bb83cd97998374fc
James Knight <git@jdknight.me>
2024-11-16 21:06:57 -0500

    tests: verify licensing building pulls appropriate version descriptions
    Signed-off-by: James Knight <james.d.knight@live.com>

--- tests/unit-tests/test_licenses.py
+++ tests/unit-tests/test_licenses.py
@@ -94,3 +94,28 @@ class TestLicenses(RelengToolTestCase):
             # verify we have no license information if packages have no license
             # information to provide
             self.assertFalse(license_cache)
+
+    def test_licenses_version(self):
+        pkg_names = [
+            'test-c',
+            'test-d',
+        ]
+
+        expected_version_desc = [
+            '2.0',
+            '3.0',
+        ]
+
+        with prepare_testenv(template='licenses') as engine:
+            pkgs = engine.pkgman.load(pkg_names)
+
+            license_manager = LicenseManager(engine.opts)
+            license_cache = license_manager.build_cache(pkgs)
+
+            for pkg, expected in zip(pkg_names, expected_version_desc):
+                # a package with license information should be tracked
+                self.assertTrue(pkg in license_cache)
+
+                # verify we have expected version description
+                version_desc = license_cache[pkg]['version']
+                self.assertEqual(version_desc, expected)
