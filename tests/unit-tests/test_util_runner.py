205ef4d5b5a2e788c0a91700b18bb46e147a14b7
James Knight <git@jdknight.me>
2025-01-18 13:05:05 -0500

    tests: verify ci runner debug mode check
    Signed-off-by: James Knight <git@jdknight.me>

---
+++ tests/unit-tests/test_util_runner.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright releng-tool
+# SPDX-License-Identifier: BSD-2-Clause
+
+from releng_tool.util.runner import detect_ci_runner_debug_mode
+from tests import RelengToolTestCase
+import os
+
+
+class TestUtilRunner(RelengToolTestCase):
+    def test_utilrunner_cidebugdetect(self):
+        with self.env_wrap():
+            # pretend we are running in a debugging mode
+            os.environ['ACTIONS_RUNNER_DEBUG'] = '1'
+
+            self.assertTrue(detect_ci_runner_debug_mode())
