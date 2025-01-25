3985830a84442da175fbdf996f35af9c1d19bd71
James Knight <git@jdknight.me>
2025-01-25 00:24:19 -0500

    switch from setuptools to hatch
    Updating the packaging tool for this project to use Hatch. Using a
    modern backend that supports multiple namespaces (for both `releng_tool`
    and `releng-tool`).
    
    Signed-off-by: James Knight <git@jdknight.me>

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright releng-tool
-# SPDX-License-Identifier: BSD-2-Clause
-
-import setuptools
-
-if __name__ == '__main__':
-    setuptools.setup()
