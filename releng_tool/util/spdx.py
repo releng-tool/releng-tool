1f540de280e37e10732544032c07a08b5a04ea35
James Knight <git@jdknight.me>
2025-01-26 16:18:37 -0500

    drop legacy Queue class
    Since releng-tool no longer supports Python 2.7, there is no need to
    use `Queue` for compatible queue handling; updating.
    
    Signed-off-by: James Knight <git@jdknight.me>

--- releng_tool/util/spdx.py
+++ releng_tool/util/spdx.py
@@ -2,12 +2,8 @@
 # Copyright releng-tool
 
 from collections.abc import Sequence
+from queue import Queue
 import re
-
-try:
-    from queue import Queue
-except ImportError:
-    from Queue import Queue
 
 
 def spdx_extract(raw):
