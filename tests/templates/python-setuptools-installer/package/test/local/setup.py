52dbb0f210f9f0ecb10c536ae26cc8fb62cd3a32
James Knight <git@jdknight.me>
2025-02-02 14:23:21 -0500

    tests: validate installer override for python packages
    Signed-off-by: James Knight <git@jdknight.me>

---
+++ tests/templates/python-setuptools-installer/package/test/local/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    description='hello world description',
+    name='hello world',
+    packages=find_packages(),
+    version='1.0',
+)
---
+++ tests/tool-tests/test_tool_python_setuptools_installer.py
@@ -0,0 +1,80 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# Copyright releng-tool
+
+from tests.support.python_tool_test import PythonSiteToolBase
+import os
+import unittest
+
+
+class TestToolPythonSetuptoolsInstaller(PythonSiteToolBase):
+    @classmethod
+    def setUpClass(cls):
+        # support skipping the test for a distribution build
+        if os.getenv('RELENG_SKIP_TEST_TOOL_PYTHON_SETUPTOOLS'):
+            raise unittest.SkipTest('skipped due to environment flag')
+
+    def tool_template(self):
+        return 'python-setuptools-installer'
+
+    def test_tool_python_setuptools_installer_default(self):
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        site_packages = self.find_site_packages(self.engine.opts.host_dir)
+        self.assertIsNone(site_packages)
+
+        site_packages = self.find_site_packages(self.engine.opts.staging_dir)
+        self.assertIsNone(site_packages)
+
+        site_packages = self.find_site_packages(self.engine.opts.target_dir)
+        self.assertIsNotNone(site_packages)
+        self.assertPythonModuleExists(site_packages, 'hello_module')
+
+    def test_tool_python_setuptools_installer_host(self):
+        self.defconfig_add('INSTALL_TYPE', 'host')
+
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        site_packages = self.find_site_packages(self.engine.opts.host_dir)
+        self.assertIsNotNone(site_packages)
+        self.assertPythonModuleExists(site_packages, 'hello_module')
+
+        site_packages = self.find_site_packages(self.engine.opts.staging_dir)
+        self.assertIsNone(site_packages)
+
+        site_packages = self.find_site_packages(self.engine.opts.target_dir)
+        self.assertIsNone(site_packages)
+
+    def test_tool_python_setuptools_installer_staging(self):
+        self.defconfig_add('INSTALL_TYPE', 'staging')
+
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        site_packages = self.find_site_packages(self.engine.opts.host_dir)
+        self.assertIsNone(site_packages)
+
+        site_packages = self.find_site_packages(self.engine.opts.staging_dir)
+        self.assertIsNotNone(site_packages)
+        self.assertPythonModuleExists(site_packages, 'hello_module')
+
+        site_packages = self.find_site_packages(self.engine.opts.target_dir)
+        self.assertIsNone(site_packages)
+
+    def test_tool_python_setuptools_installer_staging_and_target(self):
+        self.defconfig_add('INSTALL_TYPE', 'staging_and_target')
+
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        site_packages = self.find_site_packages(self.engine.opts.host_dir)
+        self.assertIsNone(site_packages)
+
+        site_packages = self.find_site_packages(self.engine.opts.staging_dir)
+        self.assertIsNotNone(site_packages)
+        self.assertPythonModuleExists(site_packages, 'hello_module')
+
+        site_packages = self.find_site_packages(self.engine.opts.target_dir)
+        self.assertIsNotNone(site_packages)
+        self.assertPythonModuleExists(site_packages, 'hello_module')
