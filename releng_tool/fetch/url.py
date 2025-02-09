be3c9fc4e7d55de12d5e9905daa5270853efface
James Knight <git@jdknight.me>
2025-02-08 22:54:07 -0500

    fetch-url: support URL fetch retries on transient errors
    Attempt to be flexible if a URL fetch fails due to a transient error.
    If detected, the system will wait a moment before retrying (up to a
    total of three attempts per site; not including a mirror configuration).
    
    Signed-off-by: James Knight <git@jdknight.me>

--- releng_tool/fetch/url.py
+++ releng_tool/fetch/url.py
@@ -5,12 +5,21 @@ from releng_tool.util.log import err
 from releng_tool.util.log import log
 from releng_tool.util.log import note
 from releng_tool.util.log import warn
+from urllib.error import HTTPError
 from urllib.request import urlopen
+import math
 import os
-
+import random
+import time
 
 #: size of blocks read when downloading a resource
 REQUEST_READ_BLOCKSIZE = 8192
+
+#: total number of retries (including initial request) before giving up
+RETRY_ATTEMPTS = 3
+
+# the maximum duration (in seconds) to wait for a retry event
+RETRY_DURATION = 10
 
 
 def fetch(opts):
@@ -28,16 +37,54 @@ def fetch(opts):
     """
 
     assert opts
-    cache_file = opts.cache_file
+    is_mirror = opts._mirror
     name = opts.name
     site = opts.site
-    is_mirror_attempt = opts._mirror
+
+    note('fetching {}...', name)
+
+    attempt = 1
+    while True:
+        cache_file, code, msg = _fetch_attempt(opts)
+        if cache_file:
+            return cache_file
+
+        # if too many attempts, stop
+        if attempt > RETRY_ATTEMPTS:
+            break
+
+        # ignore non-transient errors
+        if code not in [408, 429, 500, 502, 503, 504]:
+            break
+
+        # we still attempt a retry; first inform the user of the state
+        warn('failed to download resource: {}\n'
+            '    {}', site, msg)
+
+        # configure the delay + jitter
+        delay = RETRY_DURATION
+        delay += random.uniform(0.0, 0.5)  # noqa: S311
+
+        # wait the calculated delay before retrying again
+        reported_delay = math.ceil(delay)
+        log(f'retrying in {reported_delay} seconds...')
+        time.sleep(delay)
+        attempt += 1
+
+    log_func = warn if is_mirror else err
+    log_func('failed to download resource: {}\n'
+        '    {}', site, msg)
+    return None
+
+
+def _fetch_attempt(opts):
+    cache_file = opts.cache_file
+    site = opts.site
     urlopen_context = opts._urlopen_context
 
     filename = os.path.basename(cache_file)
 
-    note('fetching {}...', name)
-
+    read = 0
     log('requesting: ' + site)
     try:
         with urlopen(site, context=urlopen_context) as rsp:
@@ -50,7 +97,6 @@ def fetch(opts):
                 except ValueError:
                     pass
 
-            read = 0
             with open(cache_file, 'wb') as f:
                 while True:
                     buf = rsp.read(REQUEST_READ_BLOCKSIZE)
@@ -69,19 +115,17 @@ def fetch(opts):
                                 filename, read_str), end='\r')
 
                     f.write(buf)
+    except HTTPError as e:
+        return None, e.code, e
     except Exception as e:
-        log_func = warn if is_mirror_attempt else err
-        log_func('failed to download resource: {}\n'
-            '    {}', site, e)
-        return None
-
-    # cleanup any download progress prints
-    if read > 0:
-        log('')
+        return None, None, e
+    finally:
+        # cleanup any download progress prints
+        if read > 0:
+            log('')
 
     log('completed download ({})', display_size(read))
-    return cache_file
-
+    return cache_file, None, None
 
 def display_size(val):
     """
