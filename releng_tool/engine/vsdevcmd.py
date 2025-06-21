ac3a530b21767db34e2a0daa3d963320b042710e
James Knight <git@jdknight.me>
2025-06-21 16:59:44 -0400

    flexible vsdevcmd finding
    When searching for `VsDevCmd.bat` for environment options to configure,
    the process can fail if the lasted found Visual Studio package does
    not provide such a batch file (e.g. Visual Studio Test Agent). To be
    flexible in the default scan, instead of only searching for the latest
    product type, cycle through all installed products (sorted by latest
    installed) until `VsDevCmd.bat` is found.
    
    Signed-off-by: James Knight <git@jdknight.me>

--- releng_tool/engine/vsdevcmd.py
+++ releng_tool/engine/vsdevcmd.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: BSD-2-Clause
 # Copyright releng-tool
 
+from pathlib import Path
 from releng_tool.exceptions import RelengToolMissingVsDevCmdError
 from releng_tool.tool.vswhere import VSWHERE
 from releng_tool.util.io import execute_rv
@@ -14,7 +15,7 @@ def vsdevcmd_initialize(prodstr=None, verstr=None, env=None):
 
     args = (
         '-property', 'installationPath',
-        '-latest',
+        '-sort',
         '-products', prodstr or '*',
     )
     if verstr and not isinstance(verstr, bool):
@@ -30,15 +31,28 @@ def vsdevcmd_initialize(prodstr=None, verstr=None, env=None):
         raise RelengToolMissingVsDevCmdError(
             'non-existent installation path', 'no installations found')
 
-    install_dir = output
-    if not os.path.isdir(install_dir):
-        raise RelengToolMissingVsDevCmdError(
-            'non-existent installation path', install_dir)
+    detected_dirs = output.splitlines(keepends=False)
+    install_dirs = [Path(entry.strip()) for entry in detected_dirs]
+    vsdevcmd = None
 
-    debug('verifying vsdevcmd.bat exists...')
-    vsdevcmd = os.path.join(install_dir, 'Common7', 'Tools', 'VsDevCmd.bat')
-    if not os.path.isfile(vsdevcmd):
-        raise RelengToolMissingVsDevCmdError('missing VsDevCmd.bat', vsdevcmd)
+    for install_dir in install_dirs:
+        if vsdevcmd:
+            debug(f'ignoring path from vswhere: {install_dir}')
+        elif install_dir.is_dir():
+            check_vsdevcmd = install_dir / 'Common7' / 'Tools' / 'VsDevCmd.bat'
+            if check_vsdevcmd.is_file():
+                debug(f'found vsdevcmd.bat in: {install_dir}')
+                vsdevcmd = check_vsdevcmd
+            else:
+                debug(f'missing vsdevcmd.bat in: {install_dir}')
+        else:
+            verbose(f'non-existent installation path provided: {install_dir}')
+
+    if not vsdevcmd:
+        raise RelengToolMissingVsDevCmdError(
+            'unable to find VsDevCmd.bat in installation path',
+            '    '.join(install_dirs),
+        )
 
     debug('detecting vsdevcmd variables')
     comspec = os.getenv('COMSPEC', 'cmd.exe')
