12dbe3ace25446cd88eb5c08a3b9bef5c932f300
James Knight <git@jdknight.me>
2025-06-21 23:54:10 -0400

    tests: extend checks on providing profiles/quirks
    Signed-off-by: James Knight <git@jdknight.me>

---
+++ tests/unit-tests/test_opts.py
@@ -0,0 +1,117 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# Copyright releng-tool
+
+from releng_tool.opts import RelengEngineOptions
+from tests import RelengToolTestCase
+
+
+class TestMisc(RelengToolTestCase):
+    def test_opts_accept_profiles(self):
+        # default no profiles
+        opts = RelengEngineOptions()
+        self.assertFalse(opts.profiles)
+
+        # can provide a single profile
+        args = MockArgs()
+        args.profile = [
+            'example',
+        ]
+
+        opts = RelengEngineOptions(args)
+        self.assertEqual(opts.profiles, [
+            'example',
+        ])
+
+        # can provide multiple profile
+        args = MockArgs()
+        args.profile = [
+            'zulu',
+            'first',
+            'second',
+            'third',
+        ]
+
+        opts = RelengEngineOptions(args)
+        self.assertEqual(opts.profiles, [
+            'zulu',
+            'first',
+            'second',
+            'third',
+        ])
+
+        # profiles are normalized
+        args = MockArgs()
+        args.profile = [
+            ' this is;an--example',
+        ]
+
+        opts = RelengEngineOptions(args)
+        self.assertEqual(opts.profiles, [
+            'this-is-an-example',
+        ])
+
+        # only track a unique list of profiles
+        args = MockArgs()
+        args.profile = [
+            'test1',
+            'test1',
+            'test1 ',
+            ' test1',
+        ]
+
+        opts = RelengEngineOptions(args)
+        self.assertEqual(opts.profiles, [
+            'test1',
+        ])
+
+    def test_opts_accept_quirks(self):
+        # default no quirks
+        opts = RelengEngineOptions()
+        self.assertFalse(opts.quirks)
+
+        # can provide a single quirks
+        args = MockArgs()
+        args.quirk = [
+            'example',
+        ]
+
+        opts = RelengEngineOptions(args)
+        self.assertEqual(opts.quirks, [
+            'example',
+        ])
+
+        # can provide multiple quirks
+        args = MockArgs()
+        args.quirk = [
+            'zulu',
+            'first',
+            'second',
+            'third',
+        ]
+
+        opts = RelengEngineOptions(args)
+        self.assertEqual(opts.quirks, [
+            'zulu',
+            'first',
+            'second',
+            'third',
+        ])
+
+        # only track a unique list of quirks
+        args = MockArgs()
+        args.quirk = [
+            'test1',
+            'test1',
+            'test1 ',
+            ' test1',
+        ]
+
+        opts = RelengEngineOptions(args)
+        self.assertEqual(opts.quirks, [
+            'test1',
+        ])
+
+
+class MockArgs:
+    def __getattr__(self, name):
+        return self.name if name in self.__dict__ else None
