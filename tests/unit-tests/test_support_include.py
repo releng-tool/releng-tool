f90314e9b6abe20c4d3800ccba475361540bff71
James Knight <git@jdknight.me>
2026-02-05 22:20:12 -0500

    tests: add checks for releng_include
    Adding some explicit/trivial checks for the use of `releng_include`.
    
    Signed-off-by: James Knight <git@jdknight.me>

---
+++ tests/unit-tests/test_support_include.py
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# Copyright releng-tool
+
+from pathlib import Path
+from releng_tool.support import releng_include
+from tests import RelengToolTestCase
+from tests.support import fetch_unittest_assets_dir
+import os
+
+
+class TestSupport(RelengToolTestCase):
+    @classmethod
+    def setUpClass(cls):
+        assets_dir = Path(fetch_unittest_assets_dir())
+        cls.includes = assets_dir / 'includes'
+        cls.assertTrue(cls.includes.exists(), 'missing includes directory')
+
+    def test_support_include_default(self):
+        test_include = self.includes / 'sample-include.rt'
+        self.assertTrue(test_include.exists(), 'missing include file')
+        releng_include(test_include)
+
+    def test_support_include_fail(self):
+        test_include = self.includes / 'fail-include.rt'
+        self.assertTrue(test_include.exists(), 'missing include file')
+        with self.assertRaises(SystemExit):
+            releng_include(test_include)
