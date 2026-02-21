7d49454c3e46760f4c47e2071cf457a1baf9c5eb
James Knight <git@jdknight.me>
2026-02-21 09:40:47 -0500

    support cifs mounts when running coverage tests
    Allow easy overrides of coverage's data file through a
    `CUSTOM_COVERAGE_DB` environment variable. This variable can then be
    used where the working path is on a CIFS mount, where we override the
    path to an interim location to avoid database locking issues. Once
    coverage is performed, we then copy the generated coverage data to the
    working path.
    
    Related: https://github.com/coveragepy/coveragepy/issues/1993
    Signed-off-by: James Knight <git@jdknight.me>

--- scripts/task-coverage-container.sh
+++ scripts/task-coverage-container.sh
@@ -14,4 +14,7 @@ launch_script="$root_dir/support/test-container/launch"
 
 # launch container with the coverage task, starting with a clean state
 exec "$launch_script" -- \
-    /mnt/scripts/task-coverage.sh --no-report --workdir /tmp
+    /mnt/scripts/task-coverage.sh \
+        --no-report \
+        --workdir /tmp \
+        "$@"
--- scripts/task-coverage.sh
+++ scripts/task-coverage.sh
@@ -5,12 +5,17 @@
 
 set -e
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)
+root_dir=$(realpath "$script_dir/../")
 
+perform_cifs_db_tweak=false
 perform_clean=true
 perform_report=true
 new_args=()
 for arg in "$@"; do
     case "$arg" in
+    --cifs)
+        perform_cifs_db_tweak=true
+        ;;
     --no-clean)
         perform_clean=false
         ;;
@@ -26,6 +31,15 @@ done
 cmd_prefix=
 if command -v winpty >/dev/null 2>/dev/null; then
     cmd_prefix=winpty
+fi
+
+# check if we want to configure a custom database path to prevent sqlite
+# issues on a cifs store
+if [ $perform_cifs_db_tweak = true ]; then
+    printf "Setting up a CIFS database path tweak... "
+    mkdir -p /tmp/releng-tool-coverage
+    export CUSTOM_COVERAGE_DB=/tmp/releng-tool-coverage/.coverage
+    echo "done."
 fi
 
 # start with a clean state
@@ -45,4 +59,12 @@ if [ $perform_report = true ]; then
     echo "Building a coverage report..."
     $cmd_prefix tox "${new_args[@]}" \
         -c "$script_dir"/../tox-coverage.ini -e coverage-report
+fi
+
+# if we were operating in a cifs mode, relocate the database back to the
+# expected location
+if [ $perform_cifs_db_tweak = true ]; then
+    printf "Copying over coverage database back to CIFS mount... "
+    cp "${CUSTOM_COVERAGE_DB%/*}/".* "$root_dir"
+    echo "done."
 fi
