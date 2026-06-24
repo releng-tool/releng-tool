a0d8588999a89fb12e799b9a89eb98fb536f9118
James Knight <git@jdknight.me>
2026-06-23 23:09:20 -0400

    xmake: permit usage for single-user containers
    Xmake, let along any other package type used in releng-tool, should not
    be run in an elevated state to ensure misconfigurations do not cause
    issues for the host environment. Running releng-tool warns about this
    on its run. Here, we disable Xmake's check to avoid the double
    warning/error and be flexible for users running in a single-user
    container.
    
    Signed-off-by: James Knight <git@jdknight.me>

--- releng_tool/tool/xmake.py
+++ releng_tool/tool/xmake.py
@@ -6,6 +6,16 @@ from releng_tool.tool import RelengTool
 #: executable used to run xmake commands
 XMAKE_COMMAND = 'xmake'
 
+#: dictionary of environment entries append to the environment dictionary
+MESON_EXTEND_ENV = {
+    # xmake should not be run as root, but the same applies for using
+    # releng-tool in general for other package types; releng-tool comes with
+    # an elevated user check to stop or warn users already, and we do not want
+    # only the xmake utility stacking this enforcement; leave the check off
+    # to be flexible for single user container builds
+    'XMAKE_ROOT': 'y',
+}
+
 #: list of environment keys to filter from a environment dictionary
 XMAKE_SANITIZE_ENV_KEYS = [
     'XMAKE_CONFIGDIR',
@@ -14,4 +24,8 @@ XMAKE_SANITIZE_ENV_KEYS = [
 ]
 
 #: xmake host tool helper
-XMAKE = RelengTool(XMAKE_COMMAND, env_sanitize=XMAKE_SANITIZE_ENV_KEYS)
+XMAKE = RelengTool(
+    XMAKE_COMMAND,
+    env_include=MESON_EXTEND_ENV,
+    env_sanitize=XMAKE_SANITIZE_ENV_KEYS,
+)
