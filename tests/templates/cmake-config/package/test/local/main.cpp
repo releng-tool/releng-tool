bee5f89a06d2eaf0733e2e19422e58849dbcba57
James Knight <git@jdknight.me>
2026-08-01 07:35:11 -0400

    tests: introduce a cmake-config
    Replicating module tests and adjusting the template to focus on "Config"
    dependency mapping over modules.
    
    Signed-off-by: James Knight <git@jdknight.me>

---
+++ tests/templates/cmake-config/package/test/local/main.cpp
@@ -0,0 +1,7 @@
+#include "lib.h"
+
+int main() {
+    process();
+
+    return 0;
+}
---
+++ tests/tool-tests/test_tool_cmake_config.py
@@ -0,0 +1,108 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# Copyright releng-tool
+
+from tests.support.site_tool_test import TestSiteToolBase
+import os
+import sys
+
+# cmake template has two projects -- helpers to find the "lib" package
+LIBPKG_DEFDIR = os.path.join('package', 'lib')
+LIBPKG_DEFINITION = os.path.join(LIBPKG_DEFDIR, 'lib.rt')
+
+
+class TestToolCmakeConfig(TestSiteToolBase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.filename = 'testapp'
+        if sys.platform == 'win32':
+            cls.filename += '.exe'
+
+    def prepare_global_action(self):
+        return None  # use releng-tool default
+
+    def tool_template(self):
+        return 'cmake-config'
+
+    def test_tool_cmake_config_default(self):
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        bin_dir = os.path.join(self.engine.opts.staging_dir + self._prefix())
+        executable = os.path.join(bin_dir, self.filename)
+        self.assertFalse(os.path.exists(executable))
+
+        bin_dir = os.path.join(self.engine.opts.target_dir + self._prefix())
+        executable = os.path.join(bin_dir, self.filename)
+        self.assertTrue(os.path.exists(executable))
+
+    def test_tool_cmake_config_host(self):
+        self._update_install_type('host')
+
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        bin_dir = os.path.join(self.engine.opts.host_dir + self._prefix())
+        executable = os.path.join(bin_dir, self.filename)
+        self.assertTrue(os.path.exists(executable))
+
+    def test_tool_cmake_config_staging_only(self):
+        self._update_install_type('staging')
+
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        bin_dir = os.path.join(self.engine.opts.staging_dir + self._prefix())
+        executable = os.path.join(bin_dir, self.filename)
+        self.assertTrue(os.path.exists(executable))
+
+        bin_dir = os.path.join(self.engine.opts.target_dir + self._prefix())
+        executable = os.path.join(bin_dir, self.filename)
+        self.assertFalse(os.path.exists(executable))
+
+    def test_tool_cmake_config_staging_and_target(self):
+        self._update_install_type('staging_and_target')
+
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        bin_dir = os.path.join(self.engine.opts.staging_dir + self._prefix())
+        executable = os.path.join(bin_dir, self.filename)
+        self.assertTrue(os.path.exists(executable))
+
+        bin_dir = os.path.join(self.engine.opts.target_dir + self._prefix())
+        executable = os.path.join(bin_dir, self.filename)
+        self.assertTrue(os.path.exists(executable))
+
+    def test_tool_cmake_config_noinstall(self):
+        self.defconfig_add('CMAKE_NOINSTALL', value=True)
+
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+        bin_dir = os.path.join(self.engine.opts.target_dir + self._prefix())
+        executable = os.path.join(bin_dir, self.filename)
+        self.assertFalse(os.path.exists(executable))
+
+    def test_tool_cmake_config_toolchain(self):
+        self.defconfig_add('CONF_DEFS', {
+            'CMAKE_TOOLCHAIN_FILE': '$ROOT_DIR/toolchain.cmake',
+        })
+
+        rv = self.engine.run()
+        self.assertTrue(rv)
+
+    def _prefix(self):
+        prefix = os.path.join(self.engine.opts.sysroot_prefix, 'bin')
+        if not prefix.startswith(os.sep):
+            prefix = os.sep + prefix
+        return prefix
+
+    def _update_install_type(self, install_type):
+        self.defconfig_add('INSTALL_TYPE', install_type)
+
+        defconfig = os.path.join(self.engine.opts.root_dir, LIBPKG_DEFINITION)
+        self.assertTrue(os.path.exists(defconfig))
+        self.defconfig_add('INSTALL_TYPE', install_type,
+            defconfig=defconfig, pkg_name='lib')
